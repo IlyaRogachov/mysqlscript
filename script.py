19939ed974b25890d8f09a6c0cf60de9d570e3b7
rogachov <irohachov@n-ix.com>
2021-02-24 23:22:20 +0200

    Ya sozdal server mysql i parol

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
 include mysql-library
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="192.168.56.104",
+        user="ilya",
+        password="rognarock",
+        database="ilyadb",
+    ) 
