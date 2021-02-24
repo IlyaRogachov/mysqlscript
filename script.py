17dd7fea21a466cb0dd8a5d8cb95a03710f7e1eb
rogachov <irohachov@n-ix.com>
2021-02-24 23:25:32 +0200

    zakonchil script Script has been finished

--- script.py
+++ script.py
@@ -8,4 +8,11 @@ try:
         user="ilya",
         password="rognarock",
         database="ilyadb",
-    ) 
+    ) as connection:
+        show_db_query = "SHOW DATABASES"
+        with connection.cursor() as cursor:
+            cursor.execute(show_db_query)
+            for db in cursor:
+                   print(db)
+except Error as e:
+    print(e) 
